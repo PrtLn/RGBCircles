f39dd3a061bac20d5b3c28f2014f0ee50c215d53
oleno <pirit.lena@gmail.com>
2017-01-11 23:50:17 +0200

    adding interactions (when happens touch event)

--- app/src/main/java/com/prt/rgbcircles/GameManager.java
+++ app/src/main/java/com/prt/rgbcircles/GameManager.java
@@ -1,8 +1,5 @@
 package com.prt.rgbcircles;
 
-import android.graphics.Canvas;
-import android.graphics.Paint;
-
 public class GameManager {
     private MainCircle mainCircle;
     private CanvasView canvasView;
@@ -16,11 +13,23 @@ public class GameManager {
         initMainCircle();
     }
 
+    public static int getWidth() {
+        return width;
+    }
+
+    public static int getHeight() {
+        return height;
+    }
+
     private void initMainCircle() {
         mainCircle = new MainCircle(width / 2, height / 2);
     }
 
     public void onDraw() {
         canvasView.drawCircle(mainCircle);
+    }
+
+    public void onTouchEvent(int x, int y) {
+        mainCircle.moveMainCircleWhenTouchAt(x, y);
     }
 }
