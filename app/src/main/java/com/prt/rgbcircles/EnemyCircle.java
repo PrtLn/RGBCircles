89f0ad3a8245a746929759ff8dec19633c54b066
oleno <pirit.lena@gmail.com>
2017-01-12 14:24:56 +0200

    adding blue color to main circle and red color to enemy circle

--- app/src/main/java/com/prt/rgbcircles/EnemyCircle.java
+++ app/src/main/java/com/prt/rgbcircles/EnemyCircle.java
@@ -1,10 +1,13 @@
 package com.prt.rgbcircles;
 
+import android.graphics.Color;
+
 import java.util.Random;
 
 public class EnemyCircle extends SimpleCircle {
     public static final int FROM_RADIUS = 10;
     public static final int TO_RADIUS = 110;
+    private static final int ENEMY_CIRCLE = Color.RED;
 
     public EnemyCircle(int x, int y, int radius) {
         super(x, y, radius);
@@ -16,6 +19,7 @@ public class EnemyCircle extends SimpleCircle {
         int y = random.nextInt(GameManager.getHeight());
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
         EnemyCircle enemyCircle = new EnemyCircle(x, y, radius);
+        enemyCircle.setColor(ENEMY_CIRCLE);
         return enemyCircle;
     }
 }
--- app/src/main/java/com/prt/rgbcircles/MainCircle.java
+++ app/src/main/java/com/prt/rgbcircles/MainCircle.java
@@ -1,11 +1,15 @@
 package com.prt.rgbcircles;
 
+import android.graphics.Color;
+
 public class MainCircle extends SimpleCircle {
     private static final int INIT_RADIUS = 50;
     public static final int MAIN_SPEED = 30;
+    public static final int OUR_COLOR = Color.BLUE;
 
     public MainCircle(int x, int y) {
         super(x, y, INIT_RADIUS);
+        setColor(OUR_COLOR);
     }
 
     public void moveMainCircleWhenTouchAt(int x1, int y1) {
